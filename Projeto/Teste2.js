20cea5b567e496fb5e1f91a54f743d8f95293a8c
Lucas <lucasksleal@gmail.com>
2024-12-11 15:30:07 -0300

    Meu primeiro teste

---
+++ Projeto/Teste2.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+//const path = require("path")
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/teste.html")
+})
+
+app.get("/blog/:nome",function(req,res){
+    res.sendFile(__dirname + "/rotanome.html")
+})
+
+app.get("/ola/:cargo/:nome",function(req,res){
+    res.send("<h1>Ola "+req.params.nome+ "</h1>"+"<h2>Seu cargo: "+req.params.cargo+ "</h2>")
+    //o send só pode ser usado uma única vez numa rota
+})
+
+app.listen(3001, function(){
+    console.log("Servidor rodando na url http://localhost:3001")
+})
--- Teste.js
+++ Teste.js
@@ -1,1 +1,9 @@
-console.log("Olá Mundo")
+//var soma = require("./Soma")
+//console.log("Olá Mundo")
+//console.log(soma(3,4))
+
+var http = require('http')
+http.createServer(function(req,res){
+    res.end("Ola")
+}).listen(3000)
+console.log("Servidor rodando")
